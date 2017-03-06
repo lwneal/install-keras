2758bf89a436ce8177663de232a37419cec51289
Lawrence Neal <nealla@lwneal.com>
2017-03-06 11:48:32 -0800

    Add dummy script

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo "This is a dummy script for testing"
+
+echo "TODO: Check OS version"
+
+echo "TODO: lshw to check for an nvidia card"
+
+echo "TODO: check for and install nvidia drivers"
+
+echo "TODO: check for and install CUDA"
+
+echo "TODO: locate CUDA, set PATH and LD_LIBRARY_PATH"
+
+echo "TODO: download(?) and install CuDNN"
+
+echo "TODO: Install virtualenv (NOTE: use easy_install for ubuntu 14.04 compat)"
+
+echo "TODO: install tensorflow, keras, Python dependencies"
+
+echo "TODO: install h5py (pip for 16.04, apt-get python-h5py for 14.04)"
+
+echo "TODO: self-test and demo"
